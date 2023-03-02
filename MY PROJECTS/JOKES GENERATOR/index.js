25220a793f49e10d768aef990357e0a081d7e88e
PrameelaRamineni <prameela.sathish215@gmail.com>
2023-03-02 16:27:42 +0530

    my joke-project

---
+++ MY PROJECTS/JOKES GENERATOR/index.js
@@ -0,0 +1,40 @@
+const btnEl=document.getElementById("btn");
+const jokeEl=document.getElementById("joke");
+
+const apiKey="Q3SJiE1JTK5DR/tvjw1IDw==KpKjE9VTOMnZy0ZL";
+
+const options={
+    method:"GET",
+    headers:{
+        "X-Api-Key":apiKey,
+    },
+};
+
+const apiURL="https://api.api-ninjas.com/v1/dadjokes?limit=1";
+
+async function getJoke(){
+    try {
+
+        jokeEl.innerText="Updating...";
+        btnEl.disabled=true;
+        btnEl.innerText="Loading...";
+
+        const response=await fetch(apiURL,options);
+        const data=await response.json();
+    
+        btnEl.disabled=false;
+        btnEl.innerText="Tell me a joke";
+    
+       jokeEl.innerText=data[0].joke;
+        
+    } catch (error) {
+        jokeEl.innerText="An error happened,try again later";
+       
+        btnEl.disabled=false;
+        btnEl.innerText="Tell me a joke";
+        console.log(error);
+    }
+    
+}
+
+btnEl.addEventListener("click",getJoke);
