0022ef10e65ed80ce562a403cbeba1b66693a286
PrameelaRamineni <prameela.sathish215@gmail.com>
2023-10-01 11:06:38 +0530

    create a function

---
+++ JS Arrays&Objects/Create a Function.js
@@ -0,0 +1,7 @@
+//Write your code here and return the output.
+function getNationalBird(){
+    getNationalBird="Peacock" 
+    return getNationalBird;
+ }     
+let nationalBird = getNationalBird();
+console.log(nationalBird);
