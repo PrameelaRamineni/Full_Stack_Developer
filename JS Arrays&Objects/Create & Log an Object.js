e6ce2356de1a6986c944039dc674ef55459d36a0
PrameelaRamineni <prameela.sathish215@gmail.com>
2023-10-07 19:23:59 +0530

    create and log an object

---
+++ JS Arrays&Objects/Create & Log an Object.js
@@ -0,0 +1,13 @@
+// Write your code here and log the output.
+let movie={
+    title: "Baahubali",
+    director: "Rajamouli",
+    producers: ["Sobhu","Raghavendra","Prasad"],
+    'lead actors with their roles': {
+        prabhas: "Baahubali",
+        anushka: "Devasena",
+        rana: "Bhallaladeva",
+        tamannah: "Avanthika",
+    },
+};
+console.log(movie);
