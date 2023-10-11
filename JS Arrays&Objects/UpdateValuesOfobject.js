c9c5358643d2af1b800b54a082b12ee2d271b799
PrameelaRamineni <prameela.sathish215@gmail.com>
2023-10-11 19:46:59 +0530

    update the values of an Object

---
+++ JS Arrays&Objects/UpdateValuesOfobject.js
@@ -0,0 +1,45 @@
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map((str) => str.trim());
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+function main() {
+  // objectKey
+  let objectKey = readLine();
+  // val
+  let val = JSON.parse(readLine().replace(/'/g, '"'));
+  // cat
+  let cat = {
+    name: "lizzie",
+    age: 18,
+    "fur color": "grey",
+    likes: ["catnip", "milk"],
+    birthday: { month: 7, day: 17, year: 1994 }
+  };
+
+  // Write your code here and log the output.
+   cat[objectKey]=val;
+   console.log(cat);
+
+}
+  
