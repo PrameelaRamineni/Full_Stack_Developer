8bd43756e688768a15814c79ca3b4cf15705ee08
PrameelaRamineni <prameela.sathish215@gmail.com>
2023-10-24 18:55:39 +0530

    Eligibility To Vote

---
+++ JS Arrays&Objects/EligibilityToVote.js
@@ -0,0 +1,33 @@
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map((str) => str.trim());
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+function main() {
+  // arrayOfPersons
+  let arrayOfPersons = JSON.parse(readLine().replace(/'/g, '"'));
+
+  /*
+   *Write your code here and log the output.
+   */
+}
