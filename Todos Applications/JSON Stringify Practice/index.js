b6175c94b7ec598c8ffdded72e64ece69f970ef0
PrameelaRamineni <prameela.sathish215@gmail.com>
2023-11-13 16:16:31 +0530

    JSON Stringify Practice

---
+++ Todos Applications/JSON Stringify Practice/index.js
@@ -0,0 +1,48 @@
+let bikes = ["Hero", "Honda", "Bajaj", "Suzuki", "Yamaha"];
+let person = {
+    name: "Rahul",
+    age: 25,
+    gender: "Male",
+};
+let todos = [{
+        todo: "Attending CCBP sessions",
+        todoStatus: "Completed",
+    },
+    {
+        todo: "Completing practice sets",
+        todoStatus: "Not Completed",
+    },
+    {
+        todo: "Asking doubts",
+        todoStatus: "Completed",
+    },
+];
+
+
+let valuesToStringify = [bikes, person, todos]; //Store all the values in a array
+
+let jsonContainerEl = document.getElementById("jsonContainer"); //Get the jsonContainer from HTML.
+
+//In this function we create <div> and <span> elements and apply stringiedValue as textContent to span element.
+function createAndAppendValue(stringifiedValue) {
+    let valueContainerEl = document.createElement("div");
+    valueContainerEl.classList.add("value-container");
+    jsonContainerEl.appendChild(valueContainerEl);
+
+
+    let valueEl = document.createElement("span");
+    valueEl.textContent = stringifiedValue;
+    valueEl.classList.add("value");
+    valueContainerEl.appendChild(valueEl);
+}
+
+//In convertToJSONString() we stringify the value and call createAndAppendValue()
+function convertToJSONString(value) {
+    let stringifiedValue = JSON.stringify(value); //Stringify the values of each element 
+    createAndAppendValue(stringifiedValue);
+}
+
+//Iterate it over all the values 
+for (let value of valuesToStringify) { //Iterating over each element(value) of valuesToStringify
+    convertToJSONString(value);
+}
