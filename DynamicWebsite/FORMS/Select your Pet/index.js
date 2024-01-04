dc23114e56ebfe89630617e4f6215ce45263ed6d
PrameelaRamineni <prameela.sathish215@gmail.com>
2024-01-04 19:32:37 +0530

    Select your pet

---
+++ DynamicWebsite/FORMS/Select your Pet/index.js
@@ -0,0 +1,17 @@
+let petsImageUrls = {
+    dog: "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/select-your-pet-dog-img.png",
+    cat: "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/select-your-pet-cat-img.png",
+    parrot: "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/select-your-pet-parrot-img.png",
+    spider: "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/select-your-pet-spider-img.png",
+    rabbit: "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/select-your-pet-rabbit-img.png"
+};
+
+let sel = document.getElementById('petSelect');
+let image = document.getElementById("petImg");
+
+function pasteimg(event) {
+    let s = event.target.value;
+    //console.log(petsImageUrls[s])
+    image.src = petsImageUrls[s];
+}
+sel.addEventListener("change", pasteimg);
