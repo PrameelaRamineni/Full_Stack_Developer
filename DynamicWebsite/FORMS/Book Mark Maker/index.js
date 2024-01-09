c7e5c07753cbbadfb978444a39bf9a541dc0573e
PrameelaRamineni <prameela.sathish215@gmail.com>
2024-01-09 20:44:51 +0530

    Bookmark Maker

---
+++ DynamicWebsite/FORMS/Book Mark Maker/index.js
@@ -0,0 +1,44 @@
+// Example Valid URLs: https://learning.ccbp.in/, https://www.google.com/
+let bookmarkForm = document.getElementById("bookmarkForm");
+let siteNameInput = document.getElementById("siteNameInput");
+let siteUrlInput = document.getElementById("siteUrlInput");
+let submitBtn = document.getElementById("submitBtn");
+let siteNameErrMsg = document.getElementById("siteNameErrMsg");
+let siteUrlErrMsg = document.getElementById("siteUrlErrMsg");
+let bookmarksList = document.getElementById("bookmarksList");
+
+function addBookmark() {
+    let siteName = siteNameInput.value;
+    let siteURL = siteUrlInput.value;
+    let listItem = document.createElement("li");
+    bookmarksList.appendChild(listItem);
+    let bookmarkName = document.createElement("p");
+    let bookmarkurl = document.createElement("a");
+    bookmarkName.textContent = siteName;
+    bookmarkurl.textContent = siteURL;
+    bookmarkurl.href = siteURL;
+    bookmarkurl.setAttribute("target", "_blank");
+    bookmarksList.classList.toggle("d-none");
+    listItem.appendChild(bookmarkName);
+    listItem.appendChild(bookmarkurl);
+}
+
+siteUrlInput.addEventListener("change", function(event) {
+    if (event.target.value === "") {
+        siteUrlErrMsg.textContent = "Required*";
+    } else {
+        siteUrlErrMsg.textContent = "";
+
+    }
+});
+siteNameInput.addEventListener("change", function(event) {
+    if (event.target.value === "") {
+        siteNameErrMsg.textContent = "Required*";
+    } else {
+        siteNameErrMsg.textContent = "";
+    }
+});
+bookmarkForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    addBookmark();
+});
