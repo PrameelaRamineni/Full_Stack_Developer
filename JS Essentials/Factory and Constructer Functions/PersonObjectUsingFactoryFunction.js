d318566b20433238c148c3ba39e46144e2cf5772
PrameelaRamineni <prameela.sathish215@gmail.com>
2024-02-05 12:14:19 +0530

    person object using factory function

---
+++ JS Essentials/Factory and Constructer Functions/PersonObjectUsingFactoryFunction.js
@@ -0,0 +1,49 @@
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map((str) => str.trim());
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/* Please do not modify anything above this line */
+
+function createPerson(firstName, lastName) {
+  /*
+   * Write your code here and return output.
+   */
+   return{
+       firstName,
+       lastName,
+   };
+}
+
+/* Please do not modify anything below this line */
+
+function main() {
+  let firstName = readLine();
+  let lastName = readLine();
+/* Please do not modify anything above this line */ 
+  
+  /*
+   * Write your code here and log output.
+   */
+   console.log(`${firstName} ${lastName}`);
+}
