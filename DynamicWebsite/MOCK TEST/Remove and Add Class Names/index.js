ef23bacc3bacdaaef12e8ee004d5f8a64d0ce820
PrameelaRamineni <prameela.sathish215@gmail.com>
2024-06-25 22:55:39 +0530

    Remove and Add Class names

---
+++ DynamicWebsite/MOCK TEST/Remove and Add Class Names/index.js
@@ -0,0 +1,7 @@
+let btnEl=document.getElementById("btnEl");
+let heading=document.getElementById("heading");
+
+btnEl.onclick=function(){
+    heading.classList.remove("old-new-styles");
+    heading.classList.add("heading-new-styles");
+}
